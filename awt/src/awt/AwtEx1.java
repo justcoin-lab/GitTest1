6e86a7e6dfadb9331e58a74e7fab7427822264a5
justcoin-lab <justcoin@naver.com>
2025-02-12 02:48:47 +0900

    Commit 5

--- awt/src/awt/AwtEx1.java
+++ awt/src/awt/AwtEx1.java
@@ -8,7 +8,7 @@ public class AwtEx1 extends Frame{
 	
 	AwtEx1(String title){
 		super(title); // 타이틀, 창 위쪽에 출력되는 문장
-		setSize(800,300); // x, y 축 길이, 여기선 300 * 300 정사각형
+		setSize(200,300); // x, y 축 길이, 여기선 300 * 300 정사각형
 		setLocation(400,200);  // 창이 출력될 위치 , x : 400, y : 200 픽셀
 		setVisible(true); // 창이 보여질 것이냐? true 보여짐, false 안보여짐
 
