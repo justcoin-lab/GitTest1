a1de5925b8a87d53a099a3ab5acfc61969a91310
justcoin-lab <justcoin@naver.com>
2025-02-11 15:07:34 +0900

    Update AwtEx1.java

--- awt/src/awt/AwtEx1.java
+++ awt/src/awt/AwtEx1.java
@@ -11,6 +11,8 @@ public class AwtEx1 extends Frame{
 		setSize(300,300); // x, y 축 길이, 여기선 300 * 300 정사각형 
 		setLocation(400,200);  // 창이 출력될 위치 , x : 400, y : 200 픽셀
 		setVisible(true); // 창이 보여질 것이냐? true 보여짐, false 안보여짐
+
+		///깃 업로드 테스트
 	}
 
 	public static void main(String[] args) {
